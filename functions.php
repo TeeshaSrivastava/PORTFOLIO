9f1ef0db3e8e7e18dca2b174238b93cdc78a8f84
TeeshaSrivastava <2k22.csai.32521@gmail.com>
2025-07-21 00:17:05 +0530

    Delete functions.php

--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function teesha_portfolio_enqueue_scripts() {
-    wp_enqueue_style('style', get_stylesheet_uri());
-    wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css');
-}
-add_action('wp_enqueue_scripts', 'teesha_portfolio_enqueue_scripts');
-?>
